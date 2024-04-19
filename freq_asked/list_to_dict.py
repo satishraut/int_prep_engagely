9fb1050f6bf35c9414ffb630228667063124433c
SatishRaut121 <satish.raut@adcreators.com.au>
2024-04-19 16:32:44 +0530

    added fetures

---
+++ freq_asked/list_to_dict.py
@@ -0,0 +1,6 @@
+#convert two lists into a dictionary
+list1=[1,2,3,4]
+list2=[5,6,7,8]
+
+dict1=dict(zip(list1,list2))
+print("Dictionary created using zip function:",dict1)
--- welcome.py
+++ welcome.py
@@ -1,2 +1,18 @@
-print("Hello")
-print("this is second line added")
+# can i practice mysql queries here
+import mysql.connector as mc
+mydb = mc.connect(user='root', password='Minacs@123',
+                              host='127.0.0.1', database='python_db',
+                              auth_plugin='mysql_native_password') 
+cursor = mydb.cursor(buffered=True)
+#query = "CREATE DATABASE IF NOT EXISTS python_db"
+#query = "CREATE TABLE IF NOT EXISTS my_table(name varchar(255), age int)"
+#query = "INSERT INTO my_table (name, age) VALUES ('John', 30),('Satish',34)"
+query = "SELECT * FROM my_table;"
+cursor.execute(query)
+
+print(cursor.fetchall())
+mydb.commit()
+cursor.close()
+mydb.close()
+
+# now let's connect to the database and execute a query
